4f750f24b0e05fe021dde6392543f666b611d56d
Manuel della Gala <74384683+ManueldG@users.noreply.github.com>
2021-05-17 14:01:44 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+esercizio di oggi: PHP Badwords
+nome cartella / repo: php-badwords
+Ricordate di creare e mantenere la cartella di esercizio nella cartella htdocs di MAMP
+DESCRIZIONE:
+Creare una variabile con assegnato un testo per popolare un paragrafo.
+Visualizzare a schermo il paragrafo.
+Visualizzare a schermo la lunghezza di testo del paragrafo.
+Passare un parametro (query string) nella url chiamato badword con assegnato il valore di una parola scelta da voi, ottenere il suo valore con GET in PHP e sostituire ogni ricorrenza di questa parola, all'interno del testo creato da voi sopra, con tre *.
+Stampare a schermo il paragrafo con il testo censurato.
+*/
+
+?>
