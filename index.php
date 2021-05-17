968fda5836dbdd377bfd88d72a9521fb242791ef
Manuel della Gala <manuel.dellagala@gmail.com>
2021-05-17 18:09:03 +0200

    ok

--- index.php
+++ index.php
@@ -23,29 +23,34 @@ Stampare a schermo il paragrafo con il testo censurato.
     <form action="index.php" target="_self" method="post">
         <input type="text" name="censura" id="read">
     </form>
-</body>
-</html>
+
 
 
 <?
-$paragraph = <<<EOF
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vitae libero eu turpis mollis vestibulum id et nisl. Etiam nec egestas arcu. Suspendisse a cursus mauris. Aliquam interdum massa convallis diam rutrum semper. Ut dictum sed mi lobortis porttitor. Donec ultrices sapien at mollis malesuada. Nulla auctor mauris id porta lobortis. Cras ac malesuada eros, sed molestie dui. Vivamus euismod augue efficitur rutrum dictum. Phasellus leo libero, consequat non sapien ac, finibus varius nulla. Nam finibus tempor ex a placerat. Suspendisse eu mattis mauris.
+    $paragraph = <<<EOF
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vitae libero eu turpis mollis vestibulum id et nisl. Etiam nec egestas arcu. Suspendisse a cursus mauris. Aliquam interdum massa convallis diam rutrum semper. Ut dictum sed mi lobortis porttitor. Donec ultrices sapien at mollis malesuada. Nulla auctor mauris id porta lobortis. Cras ac malesuada eros, sed molestie dui. Vivamus euismod augue efficitur rutrum dictum. Phasellus leo libero, consequat non sapien ac, finibus varius nulla. Nam finibus tempor ex a placerat. Suspendisse eu mattis mauris.
 
-Vivamus ac elit vitae enim tempus semper. Fusce molestie sapien pellentesque neque egestas tristique. Integer quis dignissim sapien. Praesent egestas, dolor vel pellentesque elementum, augue lacus sodales libero, egestas interdum nisi risus in massa. Vivamus libero lectus, fringilla fringilla purus sit amet, pharetra facilisis ligula. Cras commodo mattis nisl, a ullamcorper tellus tincidunt ac. Phasellus vitae vehicula enim, viverra malesuada sem. Curabitur suscipit, sem eu feugiat tincidunt, nunc nunc sollicitudin diam, quis imperdiet libero quam id massa. Praesent elementum volutpat nunc non vulputate. Pellentesque vitae aliquam tortor, et bibendum ex. Mauris luctus justo eget erat venenatis, non vehicula lectus tincidunt. Mauris elit diam, bibendum eget lectus sit amet, condimentum pharetra quam. Pellentesque ac sem odio. Phasellus aliquet arcu tortor, a aliquet nisi fringilla id.
+    Vivamus ac elit vitae enim tempus semper. Fusce molestie sapien pellentesque neque egestas tristique. Integer quis dignissim sapien. Praesent egestas, dolor vel pellentesque elementum, augue lacus sodales libero, egestas interdum nisi risus in massa. Vivamus libero lectus, fringilla fringilla purus sit amet, pharetra facilisis ligula. Cras commodo mattis nisl, a ullamcorper tellus tincidunt ac. Phasellus vitae vehicula enim, viverra malesuada sem. Curabitur suscipit, sem eu feugiat tincidunt, nunc nunc sollicitudin diam, quis imperdiet libero quam id massa. Praesent elementum volutpat nunc non vulputate. Pellentesque vitae aliquam tortor, et bibendum ex. Mauris luctus justo eget erat venenatis, non vehicula lectus tincidunt. Mauris elit diam, bibendum eget lectus sit amet, condimentum pharetra quam. Pellentesque ac sem odio. Phasellus aliquet arcu tortor, a aliquet nisi fringilla id.
 
-Fusce pellentesque sem sed turpis semper efficitur. Vivamus at tempor tellus. Suspendisse suscipit vel lectus sed mattis. Praesent dictum lorem nisl, ut consequat libero pellentesque ac. Duis in diam ac turpis ultrices volutpat non eu arcu. Phasellus sit amet tempor eros, ut eleifend nisl. Donec luctus, dui ut rutrum laoreet, mauris enim luctus odio, porta vulputate magna elit ac turpis. Nunc consequat, sapien et ultrices faucibus, ante erat maximus nisi, vel posuere ante massa in velit. Praesent commodo tortor in eros aliquet, vel dictum orci semper.
+    Fusce pellentesque sem sed turpis semper efficitur. Vivamus at tempor tellus. Suspendisse suscipit vel lectus sed mattis. Praesent dictum lorem nisl, ut consequat libero pellentesque ac. Duis in diam ac turpis ultrices volutpat non eu arcu. Phasellus sit amet tempor eros, ut eleifend nisl. Donec luctus, dui ut rutrum laoreet, mauris enim luctus odio, porta vulputate magna elit ac turpis. Nunc consequat, sapien et ultrices faucibus, ante erat maximus nisi, vel posuere ante massa in velit. Praesent commodo tortor in eros aliquet, vel dictum orci semper.
 
-Fusce pretium odio nibh, et laoreet ligula hendrerit a. Nulla rhoncus ullamcorper mi, in malesuada enim blandit vel. Quisque auctor in tellus ut imperdiet. Pellentesque iaculis, mi sed finibus laoreet, sapien velit rhoncus diam, id bibendum turpis velit vitae lacus. Etiam dolor est, ultricies at faucibus ac, sagittis sed ipsum. Nunc at accumsan lacus. Duis lectus tortor, viverra sit amet nisi ut, tincidunt facilisis mauris. Pellentesque vestibulum nibh sit amet dapibus semper. Nulla nec dapibus tortor. Vestibulum eu urna quis risus volutpat faucibus. Mauris euismod sapien vel diam ultricies efficitur. Mauris blandit aliquet enim, id vulputate lacus vulputate id.
+    Fusce pretium odio nibh, et laoreet ligula hendrerit a. Nulla rhoncus ullamcorper mi, in malesuada enim blandit vel. Quisque auctor in tellus ut imperdiet. Pellentesque iaculis, mi sed finibus laoreet, sapien velit rhoncus diam, id bibendum turpis velit vitae lacus. Etiam dolor est, ultricies at faucibus ac, sagittis sed ipsum. Nunc at accumsan lacus. Duis lectus tortor, viverra sit amet nisi ut, tincidunt facilisis mauris. Pellentesque vestibulum nibh sit amet dapibus semper. Nulla nec dapibus tortor. Vestibulum eu urna quis risus volutpat faucibus. Mauris euismod sapien vel diam ultricies efficitur. Mauris blandit aliquet enim, id vulputate lacus vulputate id.
 
-Morbi ac nisi eget elit tristique luctus nec vitae est. Proin tempus lorem ipsum, in euismod dui pulvinar sit amet. Cras ullamcorper nulla sit amet turpis tincidunt imperdiet. Aenean nibh eros, vehicula a enim quis, gravida luctus odio. Vivamus eget placerat turpis, a tincidunt magna. Aenean hendrerit, urna non interdum facilisis, metus diam facilisis turpis, sed pellentesque leo ipsum vel lacus. Donec scelerisque ultrices turpis maximus sollicitudin. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum luctus vestibulum sem, vel tristique tellus. Phasellus vulputate luctus molestie. Vivamus tincidunt velit nunc, vel molestie libero ornare quis. Etiam interdum imperdiet enim eget iaculis. Praesent ac enim imperdiet, laoreet mi a, ornare justo.
+    Morbi ac nisi eget elit tristique luctus nec vitae est. Proin tempus lorem ipsum, in euismod dui pulvinar sit amet. Cras ullamcorper nulla sit amet turpis tincidunt imperdiet. Aenean nibh eros, vehicula a enim quis, gravida luctus odio. Vivamus eget placerat turpis, a tincidunt magna. Aenean hendrerit, urna non interdum facilisis, metus diam facilisis turpis, sed pellentesque leo ipsum vel lacus. Donec scelerisque ultrices turpis maximus sollicitudin. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum luctus vestibulum sem, vel tristique tellus. Phasellus vulputate luctus molestie. Vivamus tincidunt velit nunc, vel molestie libero ornare quis. Etiam interdum imperdiet enim eget iaculis. Praesent ac enim imperdiet, laoreet mi a, ornare justo.
 
-EOF;
+    EOF;
 
-echo ("<p>".$paragraph."</p>");
-                   
-echo ("\n<p>Lunghezza:".strlen($paragraph)."</p>");
-$censura = strip_tags($_POST['censura']);
-echo str_ireplace ( $censura , '***' , $paragraph,);
+    echo ("<p>".$paragraph."</p>");
+                    
+    echo ("\n<p>Lunghezza:".strlen($paragraph)."</p>");
 
+    echo (var_dump($_POST));
+
+    $censura = strip_tags($_POST['censura']);
+
+    echo str_ireplace ( $censura , '***' , $paragraph,);
 ?>
+
+    </body>
+</html>
 
