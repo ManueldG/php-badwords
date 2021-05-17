d73f60bfc21b797c9df06a249a5d9a9df160f42b
Manuel della Gala <manuel.dellagala@gmail.com>
2021-05-17 18:32:27 +0200

    ok

--- index.php
+++ index.php
@@ -13,7 +13,6 @@ Stampare a schermo il paragrafo con il testo censurato.
 <head>
     <link rel="stylesheet" href="http://casaalmada.hostinggratis.it/doc/css/reset.css">
     <link rel="stylesheet" href="css/style.css">
-    <script src="js/script.js"></script>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
